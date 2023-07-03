4dd8fc5b02f33df83a31cad30bbee53023c85df8
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-07-03 21:47:40 +0200

    Update Movies-api.jsx

--- src/api/Movies-api.jsx
+++ src/api/Movies-api.jsx
@@ -8,7 +8,7 @@ const getMovies = movieName => {
     },
   };
 
-  fetch(
+  return fetch(
     `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`,
     options
   )
